b27084c33954e6c47d7ddebcd621666ae6749765
leeanJP <zzzttt9@gmail.com>
2023-07-25 17:32:25 +0900

    [기능 변경] print 기능 추가

--- helloWorld2.java
+++ helloWorld2.java
@@ -5,6 +5,20 @@ public class helloWorld2 {
         System.out.print("222");
         System.out.print("222");
         System.out.print("222");
+   /*
+   *
+   * 목소리 톤
+목소리 점점 작아짐
+설명 후 정리
 
+화면 잘 보이는 지 체크
+화이트 모드
+
+따라할 때는 천천히 속도를 맞춰서
+
+이수자 평가
+테스트 진행 -
+용어들에 대한 자주 설명
+*/
     }
 }
